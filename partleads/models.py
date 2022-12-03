e0a7ffb60338b0532587ef7191b91ab563e4290d
bokdol11859 <2019147551@yonsei.ac.kr>
2022-12-03 22:01:46 +0900

    feat: Create Candidate Model

--- partleads/models.py
+++ partleads/models.py
@@ -3,31 +3,15 @@ from django.db import models
 # Create your models here.
 
 
-class BackEnd(models.Model):
-    hyunyoung = models.IntegerField(default=0)
-    junyeol = models.IntegerField(default=0)
-    suhyun = models.IntegerField(default=0)
-    hyejin = models.IntegerField(default=0)
-    seunghee = models.IntegerField(default=0)
-    sanghoon = models.IntegerField(default=0)
-    junghyun = models.IntegerField(default=0)
-    suah = models.IntegerField(default=0)
-    jiahn = models.IntegerField(default=0)
+class Candidate(models.Model):
+    POSITION = (
+        ('FE', 'FRONTEND'),
+        ('BE', 'BACKEND')
+    )
+
+    name = models.CharField(max_length=20)
+    position = models.CharField(choices=POSITION, max_length=2)
+    vote_count = models.IntegerField(default=0)
 
     def __str__(self):
-        return 'BackEnd'
-
-
-class FrontEnd(models.Model):
-    hanbi = models.IntegerField(default=0)
-    hyunyoung = models.IntegerField(default=0)
-    nayeon = models.IntegerField(default=0)
-    seonyoung = models.IntegerField(default=0)
-    chaeyeon = models.IntegerField(default=0)
-    jieun = models.IntegerField(default=0)
-    seonho = models.IntegerField(default=0)
-    chaeri = models.IntegerField(default=0)
-    youngjun = models.IntegerField(default=0)
-
-    def __str__(self):
-        return 'FrontEnd'
+        return f'{self.name}: {self.vote_count}'
