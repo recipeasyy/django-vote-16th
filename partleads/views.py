ed66f62c87e25eb40d0d375eb8dcd315fd2f3741
bokdol11859 <2019147551@yonsei.ac.kr>
2022-12-03 22:03:36 +0900

    feat: Create view for Candidate Model

--- partleads/views.py
+++ partleads/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from partleads.models import Candidate
+from partleads.serializer import CandidateSerializer
+
+
+class CandidateView(APIView):
+    def get(self, request):
+        candidates = Candidate.objects.all()
+        serializer = CandidateSerializer(candidates, many=True)
+        return Response(serializer.data)
+
+    # def post(self, request):
+
