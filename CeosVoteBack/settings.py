1c89f983b30bcba7922140ada51a14b521bbce66
bokdol11859 <2019147551@yonsei.ac.kr>
2022-12-03 22:04:42 +0900

    chore: Added restframework for installed apps

--- CeosVoteBack/settings.py
+++ CeosVoteBack/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'accounts',
     'demoday',
     'partleads',
@@ -82,8 +83,12 @@ WSGI_APPLICATION = 'CeosVoteBack.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT'),
     }
 }
 
